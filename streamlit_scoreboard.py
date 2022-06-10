a56de9134bc77ca123e6648f27456bb2686347cf
liyuanhao86 <106671250+liyuanhao86@users.noreply.github.com>
2022-06-10 21:38:06 +0200

    Update streamlit_scoreboard.py

--- streamlit_scoreboard.py
+++ streamlit_scoreboard.py
@@ -87,8 +87,8 @@ st.title("Politimesterskap i Funksjonell Fitness 2022 Leaderboard")
 
 st.image(image,width=100)
 
-ifSemiFinal = True
-ifFinal = True
+ifSemiFinal = False
+ifFinal = False
 if ifSemiFinal and ifFinal:
     option = st.selectbox(
         'Select leaderboard from the dropdown menu', ('Female First Stage', 'Male First Stage', 'Female Semi-Final', 'Male Semi-Final', 'Female Final', 'Male Final')
