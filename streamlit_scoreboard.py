54ef77bbff87a4410898012ef9e5842691202f20
liyuanhao86 <106671250+liyuanhao86@users.noreply.github.com>
2022-06-10 20:57:56 +0200

    Update streamlit_scoreboard.py

--- streamlit_scoreboard.py
+++ streamlit_scoreboard.py
@@ -152,7 +152,7 @@ elif option == 'Male Semi-Final':
     dfsf['Semi Final Total'] = dfsf['Workout 6 Points']+dfsf['First Stage Points']
     dfsf['SFScore'] = dfsf['Semi Final Total']*1000+dfsf['Total Lift']
     dfsf['Semi Final Rank'] = dfsf['SFScore'].rank(axis=0, method='min', ascending=False).astype(int)
-    tmpsf = dfsf[['Semi Final Rank', 'Semi Final Total', 'Workout 6 Points', 'Snatch', 'Clean and Jerk', 'Total Lift']].sort_values(by=['Semi Final Rank'])
+    tmpsf = dfsf[['Semi Final Rank', 'Semi Final Total', 'Workout 6 Points', 'First Stage Points', 'Snatch', 'Clean and Jerk', 'Total Lift']].sort_values(by=['Semi Final Rank'])
     st.subheader("Leaderboard")
     st.table(tmpsf)
 elif option == 'Female Semi-Final':
@@ -169,7 +169,7 @@ elif option == 'Female Semi-Final':
     dfsf['Semi Final Total'] = dfsf['Workout 6 Points']+dfsf['First Stage Points']
     dfsf['SFScore'] = dfsf['Semi Final Total']*1000+dfsf['Total Lift']
     dfsf['Semi Final Rank'] = dfsf['SFScore'].rank(axis=0, method='min', ascending=False).astype(int)
-    tmpsf = dfsf[['Semi Final Rank', 'Semi Final Total', 'Workout 6 Points', 'Snatch', 'Clean and Jerk', 'Total Lift']].sort_values(by=['Semi Final Rank'])
+    tmpsf = dfsf[['Semi Final Rank', 'Semi Final Total', 'Workout 6 Points', 'First Stage Points', 'Snatch', 'Clean and Jerk', 'Total Lift']].sort_values(by=['Semi Final Rank'])
     st.subheader("Leaderboard")
     st.table(tmpsf)
 
