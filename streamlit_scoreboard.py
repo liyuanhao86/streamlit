1551c0e0dd8e7f7234980ad0f64f0ddbc158d128
liyuanhao86 <106671250+liyuanhao86@users.noreply.github.com>
2022-06-11 13:37:08 +0200

    Update streamlit_scoreboard.py

--- streamlit_scoreboard.py
+++ streamlit_scoreboard.py
@@ -154,6 +154,7 @@ elif option == 'Male Semi-Final':
     dfsf['Semi Final Rank'] = dfsf['SFScore'].rank(axis=0, method='min', ascending=False).astype(int)
     tmpsf = dfsf[['Semi Final Rank', 'Semi Final Total', 'Workout 6 Points', 'First Stage Points', 'Snatch', 'Clean and Jerk', 'Total Lift']].sort_values(by=['Semi Final Rank'])
     st.subheader("Leaderboard")
+    
     st.table(tmpsf)
 elif option == 'Female Semi-Final':
     sheet = 'SFF'
