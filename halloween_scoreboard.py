19dc8efec11799154dec26e595f1b15ebfa4241f
liyuanhao86 <106671250+liyuanhao86@users.noreply.github.com>
2022-10-28 14:07:10 +0200

    Update halloween_scoreboard.py

--- halloween_scoreboard.py
+++ halloween_scoreboard.py
@@ -35,7 +35,7 @@ def DoScoreBoard(file, sheet, ifQF=False):
 	df = pd.read_excel(file, index_col=0, sheet_name = sheet).set_index(['Team'])
     score_matrix = pd.read_excel(file, index_col=0, sheet_name = 'ScoreMatrix').to_dict()
     score_matrix['points'][0]=0
-    df = df.fillna(0)
+	df = df.fillna(0)
 	
 	if sheet in ['ScoreM', 'ScoreF']:
 		for w in range(1,4):
