97852d452cc24e967ae5fad3e0e7589c639759fe
liyuanhao86 <106671250+liyuanhao86@users.noreply.github.com>
2023-11-03 09:48:07 +0100

    Add files via upload

--- halloween_scoreboard.py
+++ halloween_scoreboard.py
@@ -20,34 +20,235 @@ def add_bg_from_local(image_file):
     """,
     unsafe_allow_html=True
     )
-
-def DoScoreBoard(file, sheet, ifQF=False):
+def DoScoreBoardF(file, sheet, ifQF=False):
 	wod=[{},{},{},{},{}]
 	wod[0] = {
-		'Single-Unders':150,
-		'Burpee BGO': 15,
-		'Sumo DLHP':50,
-		'Burpee BGO2':15,
-		'Single-Unders2':150
+		'Reps':1
 	}
 	wod[1] = {
 		'Reps':1
 	}
 	wod[2] = {
-		'Wall walks':6,
-		'Wallballs':50,
-		'Deadlifts':30
+		'reps':1
 	}
 	wod[3] = {
-		'Row':400,
-		'T2B':40,
-		'Row2':400,
-		'HSPU':40
+		'reps':1,
 	}
 	wod[4] = {
 		'kg':1
 	}
-	wod_type = {1:'FT',2:'Other',3:'FT',4:'FT',5:'Other'}
+	wod_type = {1:'OtherFT',2:'Other',3:'FT',4:'FT',5:'Other'}
+	wod_rd = {1:1,2:1,3:3,4:1,5:1}
+	
+	df = pd.read_excel(file, index_col=0, sheet_name = sheet).set_index(['Team'])
+	score_matrix = pd.read_excel(file, index_col=0, sheet_name = 'ScoreMatrix').to_dict()
+	score_matrix['points'][0]=0
+	df = df.fillna(0)
+				
+	NWorkout = int(re.search(r"(?<=[A-z])[0-9]+$", df.columns[-1]).group(0))
+	
+	if sheet in ['FM', 'FF']:
+		for w in range(1,2):
+			df[f'WODdisplay{w}'] = ''
+			for team in df.index:
+				m = int(df[f'Minute{w}'][team])
+				s = int(df[f'Second{w}'][team])
+				r = int(df[f'Rep{w}'][team])
+				if r == 490:
+					df.loc[team,f'WODdisplay{w}'] = f'{m:02d}:{s:02d}'
+				elif r<= 150:
+					tmp = r
+					df.loc[team,f'WODdisplay{w}'] = f'{r} reps: {tmp} DUs (buy-in)'
+				elif r<= 210:
+					tmp = r-150
+					df.loc[team,f'WODdisplay{w}'] = f'{r} reps: {tmp} toes-to-bars'
+				elif r<= 240:
+					tmp = r-210
+					df.loc[team,f'WODdisplay{w}'] = f'{r} reps: {tmp} thrusters (50/35)'
+				elif r<= 280:
+					tmp = r-240
+					df.loc[team,f'WODdisplay{w}'] = f'{r} reps: {tmp} CTB pull-ups'
+				elif r<= 300:
+					tmp = r-280
+					df.loc[team,f'WODdisplay{w}'] = f'{r} reps: {tmp} thrusters (60/42.5)'
+				elif r<= 320:
+					tmp = r-300
+					df.loc[team,f'WODdisplay{w}'] = f'{r} reps: {tmp} bar muscle-ups'
+				elif r<= 330:
+					tmp = r-320
+					df.loc[team,f'WODdisplay{w}'] = f'{r} reps: {tmp} thrusters (70/50)'
+				elif r<= 340:
+					tmp = r-330
+					df.loc[team,f'WODdisplay{w}'] = f'{r} reps: {tmp} rope-climbs'
+				elif r<490:
+					tmp = r-340
+					df.loc[team,f'WODdisplay{w}'] = f'{r} reps: {tmp} DUs (buy-out)'
+	if ifQF == False:
+		df['QualifyPoints'] = 0
+	df['TotalPoints'] = df['QualifyPoints']
+	ss = ['QualifyPoints']
+	ss2 = []
+	ss3 = []
+	ss4 = []
+	for i in range(1, NWorkout+1):
+		ss.append(f'WODdisplay{i}')
+		m = 'Minute'+str(i)
+		s = 'Second'+str(i)
+		r = 'Rep'+str(i)
+		rankvar = 'rank'+str(i)
+		ss.append('points'+str(i))
+		df['Score'+str(i)] = df[r]*10000-df[m]*60-df[s]
+		df[rankvar] = df['Score'+str(i)].rank(axis=0, method='min', ascending=False)
+		df.loc[df[r] == 0, rankvar] = 0
+		df['points'+str(i)] = 0
+		for j in df.index:
+			df.loc[j, 'points'+str(i)] = score_matrix['points'][df.loc[j, rankvar]]
+		df['TotalPoints'] += df['points'+str(i)]
+	ss.append('TotalPoints')
+	for i in range(1, NWorkout+1):
+		if df['points'+str(i)].max() > 0:
+			ss2.append('points'+str(i))
+			ss3.append('Workout '+str(i))
+			ss4.append('WODdisplay'+str(i))
+	scoreboard = df[['rank1']+ss]
+	TB = df[ss2].min(axis = 1)
+	TB.name = "WorstRound"
+	TB2 = df[ss2].max(axis = 1)
+	TB2.name = "BestRound"
+	scoreboard = scoreboard.merge(TB, how='left', left_index=True, right_index=True)
+	scoreboard = scoreboard.merge(TB2, how='left', left_index=True, right_index=True)
+	scoreboard['PointsWithTB'] = scoreboard['TotalPoints']+0.00*scoreboard['WorstRound']+0.0000*scoreboard['BestRound']
+	TotalRank = scoreboard['TotalPoints'].rank(axis=0, method='min', ascending=False)
+	TotalRank.name = "TotalRank"
+	TotalRankTB = scoreboard['PointsWithTB'].rank(axis=0, method='min', ascending=False)
+	TotalRankTB.name = "TotalRankTB"
+	scoreboard = scoreboard.merge(TotalRank, how='left', left_index=True, right_index=True)
+	scoreboard = scoreboard.merge(TotalRankTB, how='left', left_index=True, right_index=True)
+	display = scoreboard[['TotalRankTB']+['QualifyPoints']+['rank1']+ss2+['TotalPoints']+['WODdisplay1']+['WorstRound']+['BestRound']]
+	display = display.sort_values(by=['TotalRankTB'])
+	display.columns = ['Rank']+['Qualify']+['rank1']+ss3+['Total']+['WODdisplay1']+['WorstRound']+['BestRound']
+	display = display.rename(columns={'WODdisplay1': 'Result', 'Workout 1': 'Final Pts', 'rank1': 'Final Rank'})
+	display['Rank'] = display['Rank'].astype(int)
+	display['Final Rank'] = display['Final Rank'].astype(int)
+	scoreboard['Rank'] = display['Rank'].astype(int)
+	woddis = scoreboard[ss4+ss2]
+	if ifQF:
+		return display, scoreboard, woddis
+	else:
+		return display.drop(columns=['Qualify']), scoreboard, woddis
+
+def DoScoreBoardSF(file, sheet, ifQF=False):
+	wod=[{},{},{},{},{}]
+	wod[0] = {
+		'Reps':1
+	}
+	wod[1] = {
+		'Reps':1
+	}
+	wod[2] = {
+		'reps':1
+	}
+	wod[3] = {
+		'reps':1,
+	}
+	wod[4] = {
+		'kg':1
+	}
+	wod_type = {1:'OtherFT',2:'Other',3:'FT',4:'FT',5:'Other'}
+	wod_rd = {1:1,2:1,3:3,4:1,5:1}
+	
+	df = pd.read_excel(file, index_col=0, sheet_name = sheet).set_index(['Team'])
+	score_matrix = pd.read_excel(file, index_col=0, sheet_name = 'ScoreMatrix').to_dict()
+	score_matrix['points'][0]=0
+	df = df.fillna(0)
+				
+	NWorkout = int(re.search(r"(?<=[A-z])[0-9]+$", df.columns[-1]).group(0))
+	
+	if sheet in ['SFM', 'SFF']:
+		for w in range(1,2):
+			df[f'WODdisplay{w}'] = ''
+			for team in df.index:
+				m = df[f'Minute{w}'][team]
+				s = df[f'Second{w}'][team]
+				r = df[f'Rep{w}'][team]
+				cj = df['CJ'][team]
+				snatch = df['Snatch'][team]
+				total = cj + snatch
+				df.loc[team,f'WODdisplay{w}']= f'Snatch {snatch} + C&J {cj} = {total}kg'
+
+	if ifQF == False:
+		df['QualifyPoints'] = 0
+	df['TotalPoints'] = df['QualifyPoints']
+	ss = ['QualifyPoints']
+	ss2 = []
+	ss3 = []
+	ss4 = []
+	for i in range(1, NWorkout+1):
+		ss.append(f'WODdisplay{i}')
+		m = 'Minute'+str(i)
+		s = 'Second'+str(i)
+		r = 'Rep'+str(i)
+		rankvar = 'rank'+str(i)
+		ss.append('points'+str(i))
+		df['Score'+str(i)] = df[r]*10000-df[m]*60-df[s]
+		df[rankvar] = df['Score'+str(i)].rank(axis=0, method='min', ascending=False)
+		df.loc[df[r] == 0, rankvar] = 0
+		df['points'+str(i)] = 0
+		for j in df.index:
+			df.loc[j, 'points'+str(i)] = score_matrix['points'][df.loc[j, rankvar]]
+		df['TotalPoints'] += df['points'+str(i)]
+	ss.append('TotalPoints')
+	for i in range(1, NWorkout+1):
+		if df['points'+str(i)].max() > 0:
+			ss2.append('points'+str(i))
+			ss3.append('Workout '+str(i))
+			ss4.append('WODdisplay'+str(i))
+	scoreboard = df[['rank1']+ss]
+	TB = df[ss2].min(axis = 1)
+	TB.name = "WorstRound"
+	TB2 = df[ss2].max(axis = 1)
+	TB2.name = "BestRound"
+	scoreboard = scoreboard.merge(TB, how='left', left_index=True, right_index=True)
+	scoreboard = scoreboard.merge(TB2, how='left', left_index=True, right_index=True)
+	scoreboard['PointsWithTB'] = scoreboard['TotalPoints']+0.00*scoreboard['WorstRound']+0.0000*scoreboard['BestRound']
+	TotalRank = scoreboard['TotalPoints'].rank(axis=0, method='min', ascending=False)
+	TotalRank.name = "TotalRank"
+	TotalRankTB = scoreboard['PointsWithTB'].rank(axis=0, method='min', ascending=False)
+	TotalRankTB.name = "TotalRankTB"
+	scoreboard = scoreboard.merge(TotalRank, how='left', left_index=True, right_index=True)
+	scoreboard = scoreboard.merge(TotalRankTB, how='left', left_index=True, right_index=True)
+	display = scoreboard[['TotalRankTB']+['QualifyPoints']+['rank1']+ss2+['TotalPoints']+['WODdisplay1']+['WorstRound']+['BestRound']]
+	display = display.sort_values(by=['TotalRankTB'])
+	display.columns = ['Rank']+['Qualify']+['rank1']+ss3+['Total']+['WODdisplay1']+['WorstRound']+['BestRound']
+	display = display.rename(columns={'WODdisplay1': 'Result', 'Workout 1': 'SF Pts', 'rank1': 'SF Rank'})
+	display['Rank'] = display['Rank'].astype(int)
+	display['SF Rank'] = display['SF Rank'].astype(int)
+	scoreboard['Rank'] = display['Rank'].astype(int)
+	woddis = scoreboard[ss4+ss2]
+	if ifQF:
+		return display, scoreboard, woddis
+	else:
+		return display.drop(columns=['Qualify']), scoreboard, woddis
+
+
+def DoScoreBoard(file, sheet, ifQF=False):
+	wod=[{},{},{},{},{}]
+	wod[0] = {
+		'Reps':1
+	}
+	wod[1] = {
+		'Reps':1
+	}
+	wod[2] = {
+		'reps':1
+	}
+	wod[3] = {
+		'reps':1,
+	}
+	wod[4] = {
+		'kg':1
+	}
+	wod_type = {1:'OtherFT',2:'Other',3:'FT',4:'FT',5:'Other'}
 	wod_rd = {1:1,2:1,3:3,4:1,5:1}
 	
 	df = pd.read_excel(file, index_col=0, sheet_name = sheet).set_index(['Team'])
@@ -60,11 +261,44 @@ def DoScoreBoard(file, sheet, ifQF=False):
 	if sheet in ['ScoreM', 'ScoreF']:
 		for w in range(1,NWorkout+1):
 			df[f'WODdisplay{w}'] = ''
-			for team in df.index:
-				m = df[f'Minute{w}'][team]
-				s = df[f'Second{w}'][team]
-				r = df[f'Rep{w}'][team]
-				df.loc[team,f'WODdisplay{w}']=get_wod_display(wod[w-1], m, s, r, wod_type[w], total_rd=wod_rd[w])
+			if w ==1:
+				for team in df.index:
+					m = df[f'Minute{w}'][team]
+					s = df[f'Second{w}'][team]
+					r = df[f'Rep{w}'][team]
+					df.loc[team,f'WODdisplay{w}']=get_wod_display(wod[w-1], m, s, r, wod_type[w], total_rd=wod_rd[w])
+			if w == 2:
+				for team in df.index:
+					wb = df['WB'][team]
+					bjo = df['BJO'][team]
+					t2b = df['TTR'][team]
+					ski = df['Ski'][team]
+					df.loc[team,f'WODdisplay{w}'] = f'{wb} WB, {bjo} BJO, {t2b} T2R, {ski} Cals ski'
+			if w == 3:
+				for team in df.index:
+					m = int(df[f'Minute{w}'][team])
+					s = int(df[f'Second{w}'][team])
+					r = int(df[f'Rep{w}'][team])
+					if r == 156:
+						df.loc[team,f'WODdisplay{w}'] = f'{m:02d}:{s:02d}'
+					elif r<= 16:
+						tmp = r*2.5
+						df.loc[team,f'WODdisplay{w}'] = f'{tmp}m farmer carry lunges'
+					elif r<= 24:
+						tmp = (r-16)*2.5
+						df.loc[team,f'WODdisplay{w}'] = f'{tmp}m partner wheelbarrow'
+					elif r<= 40:
+						tmp = (r-24)*2.5
+						df.loc[team,f'WODdisplay{w}'] = f'{tmp}m front-rack lunges'
+					elif r<= 120:
+						tmp = (r-40)
+						df.loc[team,f'WODdisplay{w}'] = f'{tmp} crossover single unders'
+					elif r<= 136:
+						tmp = (r-120)*2.5
+						df.loc[team,f'WODdisplay{w}'] = f'{tmp}m DB overhead lunges'
+					elif r< 156:
+						tmp = (r-136)
+						df.loc[team,f'WODdisplay{w}'] = f'{tmp}m handstand walk'
 	if sheet in ['SFM', 'SFF']:
 		for w in range(1,3):
 			df[f'WODdisplay{w}'] = ''
@@ -108,7 +342,7 @@ def DoScoreBoard(file, sheet, ifQF=False):
 	TB2.name = "BestRound"
 	scoreboard = scoreboard.merge(TB, how='left', left_index=True, right_index=True)
 	scoreboard = scoreboard.merge(TB2, how='left', left_index=True, right_index=True)
-	scoreboard['PointsWithTB'] = scoreboard['TotalPoints']+0.01*scoreboard['WorstRound']+0.0001*scoreboard['BestRound']
+	scoreboard['PointsWithTB'] = scoreboard['TotalPoints']+0.00*scoreboard['WorstRound']+0.0000*scoreboard['BestRound']
 	TotalRank = scoreboard['TotalPoints'].rank(axis=0, method='min', ascending=False)
 	TotalRank.name = "TotalRank"
 	TotalRankTB = scoreboard['PointsWithTB'].rank(axis=0, method='min', ascending=False)
@@ -126,7 +360,7 @@ def DoScoreBoard(file, sheet, ifQF=False):
 	else:
 		return display.drop(columns=['Qualify']), scoreboard, woddis
 
-def get_wod_display(wod, mm, ss, rep, tp, total_rd = 1):
+def get_wod_display(wod, mm, ss, rep, tp, total_rd = 1, appendix = pd.DataFrame()):
 	mm = int(mm)
 	ss = int(ss)
 	rep = int(rep)
@@ -179,16 +413,22 @@ def get_wod_display(wod, mm, ss, rep, tp, total_rd = 1):
 		display = display1 + display2
 	if tp == 'Other':
 		display = f'{rep} {list(wod.keys())[0]}'
+	if tp == 'OtherFT':
+		total_rep = 224
+		if rep == total_rep:
+			display = f'{mm:02d}:{ss:02d}'
+		else:
+			display = f'{rep} reps'
 	display = re.sub('\,\s$', '', display)
 	return display
 
-bg_pic = 'halloweenwp.png'
-
+bg_pic = 'C:\\Users\\yhli1\\OneDrive - Mowi ASA\\work_files\\CrossFit\\2023 Halloween games\\halloweenwp.png'
+bg_pic = 'C:\\Users\\Yuanhao.Li\\OneDrive - Mowi ASA\\work_files\\CrossFit\\2023 Halloween games\\halloweenwp.png'
 add_bg_from_local(bg_pic)    
-image = Image.open('CFBLogo.jpg')
-image2 = Image.open('halloween.png')
+image = Image.open('C:\\Users\\Yuanhao.Li\\OneDrive - Mowi ASA\\work_files\\CrossFit\\2023 Halloween games\\CFBLogo.jpg')
+image2 = Image.open('C:\\Users\\Yuanhao.Li\\OneDrive - Mowi ASA\\work_files\\CrossFit\\2023 Halloween games\\halloween.png')
 
-file = 'Scoreboard.xlsx'
+file = 'C:\\Users\\Yuanhao.Li\\OneDrive - Mowi ASA\\work_files\\CrossFit\\2023 Halloween games\\scoreboard.xlsx'
 
 headers = {
     "selector": "th:not(.index_name)",
@@ -205,18 +445,18 @@ st.title("CrossFit Bryggen Halloween Games 2022")
 st.image(image,width=100)
 
 ifSemiFinal = True
-ifFinal = False
+ifFinal = True
 if ifSemiFinal and ifFinal:
 	option = st.selectbox(
-		'Select leaderboard from the dropdown menu', ('Female First Stage', 'Male First Stage', 'Female Semi-Final', 'Male Semi-Final', 'Female Final', 'Male Final', 'Workout 1', 'Workout 2', 'Workout 3', 'Semifinal A', 'Semifinal B')
+		'Select leaderboard from the dropdown menu', ('Female First Stage', 'Male First Stage', 'Female Semi-Final', 'Male Semi-Final', 'Female Final', 'Male Final', 'Workout 1', 'Workout 2', 'Workout 3')
 	)
 elif ifSemiFinal:
 	option = st.selectbox(
-		'Select leaderboard from the dropdown menu', ('Female First Stage', 'Male First Stage', 'Female Semi-Final', 'Male Semi-Final', 'Workout 1', 'Workout 2', 'Workout 3', 'Semifinal A', 'Semifinal B')
+		'Select leaderboard from the dropdown menu', ('Female First Stage', 'Male First Stage', 'Female Semi-Final', 'Male Semi-Final', 'Workout 1', 'Workout 2', 'Workout 3')
 	)
 else:
 	option = st.selectbox(
-		'Select leaderboard from the dropdown menu', ('Female First Stage', 'Male First Stage', 'Workout 1', 'Workout 2', 'Workout 3', 'Semifinal A', 'Semifinal B')
+		'Select leaderboard from the dropdown menu', ('Female First Stage', 'Male First Stage', 'Workout 1', 'Workout 2', 'Workout 3')
 	)
 
 if option == 'Male First Stage':
@@ -239,10 +479,10 @@ elif option == 'Female First Stage':
 		df = pd.read_excel(file, index_col=0, sheet_name = sheet)
 		st.text(f'Scoreboard is not available yet')
 		st.table(df[['Team']].style.set_table_styles([headers]).set_properties(**text))
-if option == 'Male Semi-Final':
+elif option == 'Male Semi-Final':
 	sheet = 'SFM'
 	try:
-		d, s, w = DoScoreBoard(file, sheet, True)
+		d, s, w = DoScoreBoardSF(file, sheet, True)
 		st.subheader("Leaderboard")
 		st.table(d.drop(columns=['WorstRound','BestRound']).style.set_table_styles([headers]).set_properties(**text))
 	except:
@@ -251,7 +491,25 @@ if option == 'Male Semi-Final':
 elif option == 'Female Semi-Final':
 	try:
 		sheet = 'SFF'
-		d, s, w = DoScoreBoard(file, sheet, True)
+		d, s, w = DoScoreBoardSF(file, sheet, True)
+		st.subheader("Leaderboard")
+		st.table(d.drop(columns=['WorstRound','BestRound']).style.set_table_styles([headers]).set_properties(**text))
+	except:
+		df = pd.read_excel(file, index_col=0, sheet_name = sheet)
+		st.text(f'Scoreboard is not available yet')
+elif option == 'Male Final':
+	sheet = 'FM'
+	try:
+		d, s, w = DoScoreBoardF(file, sheet, True)
+		st.subheader("Leaderboard")
+		st.table(d.drop(columns=['WorstRound','BestRound']).style.set_table_styles([headers]).set_properties(**text))
+	except:
+		df = pd.read_excel(file, index_col=0, sheet_name = sheet)
+		st.text(f'Scoreboard is not available yet')
+elif option == 'Female Final':
+	try:
+		sheet = 'FF'
+		d, s, w = DoScoreBoardF(file, sheet, True)
 		st.subheader("Leaderboard")
 		st.table(d.drop(columns=['WorstRound','BestRound']).style.set_table_styles([headers]).set_properties(**text))
 	except:
@@ -283,32 +541,4 @@ elif option in ['Workout 1', 'Workout 2', 'Workout 3']:
 		st.table(dis_w.style.set_table_styles([headers]).set_properties(**text))
 	except:
 		st.text(f'Scoreboard for male division {option} is not available yet')
-elif option in ['Semifinal A', 'Semifinal B']:
-	if option == 'Semifinal A':
-		nwod = 1
-	if option == 'Semifinal B':
-		nwod = 2
-	try:
-		sheet = 'SFF'
-		d, s, w = DoScoreBoard(file, sheet, False)
-		w = w.sort_values(by=[f'points{nwod}'], ascending=False)
-		w['CurrentRank'] = w[f'points{nwod}'].rank(axis=0, method='min', ascending=False)
-		dis_w = w[['CurrentRank', f'WODdisplay{nwod}']]
-		dis_w.columns = ['Rank', option]
-		dis_w['Rank'] = dis_w['Rank'].astype(int)
-		st.subheader('Female Division ' + option)
-		st.table(dis_w.style.set_table_styles([headers]).set_properties(**text))
-	except:
-		st.text(f'Scoreboard for female division {option} is not available yet')
-	try:
-		sheet = 'SFM'
-		d, s, w = DoScoreBoard(file, sheet, False)
-		w = w.sort_values(by=[f'points{nwod}'], ascending=False)
-		w['CurrentRank'] = w[f'points{nwod}'].rank(axis=0, method='min', ascending=False)
-		dis_w = w[['CurrentRank', f'WODdisplay{nwod}']]
-		dis_w.columns = ['Rank', option]
-		dis_w['Rank'] = dis_w['Rank'].astype(int)
-		st.subheader('Male Division ' + option)
-		st.table(dis_w.style.set_table_styles([headers]).set_properties(**text))
-	except:
-		st.text(f'Scoreboard for male division {option} is not available yet')
+
